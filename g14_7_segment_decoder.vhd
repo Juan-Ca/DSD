f5262617ad8875589dfab97ea0b657228085ad4e
Juan-Ca <juan.borges@mail.mcgill.ca>
2015-10-27 16:23:59 -0400

    Modified g14_7_segment_decoder file for lab 3

--- g14_7_segment_decoder.vhd
+++ g14_7_segment_decoder.vhd
@@ -3,3 +3,13 @@ use ieee.std_logic_1164.all;
 
 entity g14_7_segment_decoder is
 port(code : in std_logic_vector(3 downto);
+		RippleBlank_In : in std_logic;
+		RippleBlank_Out : out std_logic;
+		segments : out std_logic_vector(6 downto 0));
+end g14_7_segment_decoder;
+
+architecture imp for g14_7_segment_decoder is
+
+begin
+process(code,RippleBlank_In)
+
