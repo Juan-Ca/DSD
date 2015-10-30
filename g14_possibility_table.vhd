a99fb3e797cd0fa13afeeaa314a93f1e31fa4d05
Juan-Ca <juan.borges@mail.mcgill.ca>
2015-10-30 10:51:47 -0400

    modified the possibility table vhd

--- g14_possibility_table.vhd
+++ g14_possibility_table.vhd
@@ -4,14 +4,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity g14_possibility_table is
-port(TC_en 		: in std_logic;
-		TC_RST	: in std_logic;
-		TM_IN		: in std_logic;
-		TM_EN		: in std_logic;
+port( TC_en 	: in std_logic; -- table counter enable
+		TC_RST	: in std_logic; -- table counter reset
+		TM_IN		: in std_logic; -- table memory input data
+		TM_EN		: in std_logic; -- table memory write enable
 		clk		: in std_logic;
-		TC_LAST	: out std_logic;
+		TC_LAST	: out std_logic; -- last count flag
 		TM_ADDR	: out std_logic_vector(11 downto 0);
-		TM_OUT	: out std_logic);
+		TM_OUT	: out std_logic); -- table memory output
 end g14_possibility_table;
 
 Architecture imp of g14_possibility_table is
@@ -19,5 +19,7 @@ Architecture imp of g14_possibility_table is
 
 
 begin
+
+
 
 end imp;
