a0e27e76f1510046bb785c5e4370e8d70483cb61
Juan-Ca <juan.borges@mail.mcgill.ca>
2015-11-03 17:18:21 -0500

    Complete redesign of file

--- g14_possibility_table.vhd
+++ g14_possibility_table.vhd
@@ -23,30 +23,40 @@ signal flag : std_logic;
 
 begin
 TM_ADDR <= TC;
-
-process (clk, TM_EN, TC_RST)
-
-begin		
-		for i in 0 to 5 loop
-				for j in 0 to 5 loop
-					for m in 0 to 5 loop
-						for n in 0 to 5 loop
-								first : if (TC_RST = '1') then
-									TC <= std_logic_vector(0,12),
-									i = 0, j = 0, m = 0, n = 0;			
-								elsif(TC_RST = '0' and TM_EN = '1') then
-									second : if (clk'event and ckl = '0') then
-										addr <= std_logic_vector(to_unsigned(i,3)&to_unsigned(j,3)&to_unsigned(m,3)&to_unsigned(n,3));
-									end if;
-								end if;	
-						end loop;
-					end loop;
-				end loop;
-			if (i = 5 and j = 5 and m = 5 and n = 5) then
-				TC_LAST <= '1';
-			else then
-				TC_LAST <= '0';
-			end if;
-		end loop;
+TC_LAST <= flag;
+process (clk, TC_RST)
+begin	
+	
+	if(TC = std_logic_vector(to_unsigned(0,12)) then
+			flag <= '0'
+	elsif (TC = std_logic_vector(to_unsigned(2925,12)) then
+			flag <= '1';
+	
+	elsif ((flag = '1' or TC_RST = 1) and clk'event and clk = '1') then
+			TC <= std_logic_vector(0,12);
+	elsif ((TC = std_logic_vector(to_unsigned(365,12) or TC = std_logic_vector(to_unsigned(877,12) or 
+				  TC = std_logic_vector(to_unsigned(1389,12) or TC = std_logic_vector(to_unsigned(1901,12) or 
+				  TC = std_logic_vector(to_unsigned(2413,12))and clk'event and clk = '1') then
+				TC < = TC+512;
+	elsif ((TC = std_logic_vector(to_unsigned(45,12) or TC = std_logic_vector(to_unsigned(109,12) or 
+				  TC = std_logic_vector(to_unsigned(173,12) or TC = std_logic_vector(to_unsigned(237,12) or 
+				  TC = std_logic_vector(to_unsigned(301,12))and clk'event and clk = '1') then
+				TC <= TC+64;
+	elsif ((TC = std_logic_vector(to_unsigned(5,12)) or TC = std_logic_vector(to_unsigned(13,12) or 
+			  TC = std_logic_vector(to_unsigned(21,12) or TC = std_logic_vector(to_unsigned(29,12) or 
+			  TC = std_logic_vector(to_unsigned(37,12))and clk'event and clk = '1') then
+			TC <= TC+8;
+	elsif(clk'event and ckl = '1' and flag = '0' and TC_RST = '0') then
+			TC <= TC+1;
+	end if;
 end process;
+
+process(clk, TM_EN, TC_RST)
+begin
+		TM_OUT <= '0';
+	if (clk'event and clk = '1' and TM_EN = '1' and TC_RST = '0') then
+		TM_OUT <= TM_IN;
+	end if;
+end process;
+
 end imp;
