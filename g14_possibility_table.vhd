9d84dda4d6b753855f6b275cbe9382eec62ac0c9
Juan-Ca <juan.borges@mail.mcgill.ca>
2015-11-03 09:52:31 -0500

    Modified the possibility table file

--- g14_possibility_table.vhd
+++ g14_possibility_table.vhd
@@ -2,6 +2,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity g14_possibility_table is
 port( TC_en 	: in std_logic; -- table counter enable
@@ -16,10 +18,23 @@ end g14_possibility_table;
 
 Architecture imp of g14_possibility_table is
 
-
+signal addr : std_logic_vector(11 downto 0);
+signal flag : std_logic;
 
 begin
+TM_ADDR <= addr;
+TC_LAST <= flag;
 
+process (clk, TM_EN, TC_RST)
 
+	while (TM_EN = '1' and flag = '0')then
+		if (TC_RST = '1') then
+			addr <= std_logic_vector(to_unsigned(0,12));
+		elsif (clk'event and clock = '0') then
+			addr <= std_logic_vector(to_unsigned(1,12))+addr;
+		end if;
+		if ()
+		end if;
+	end while;
 
 end imp;
